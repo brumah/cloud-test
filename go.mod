0ca6aace108cc937ff66507b13fb19c46a911a7d
brunerMatthew <bruner.matthewm@gmail.com>
2024-03-16 17:36:33 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cloud-test
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Helloworld")
+	fmt.Println("New Request")
+}
+
+func main() {
+	http.HandleFunc("GET /", handler)
+
+	port := ":8080"
+	fmt.Printf("Server is running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
